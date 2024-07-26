0ed94da2bfa2b40366cf96f4fce77a7bf0356649
unknown <lucasvinisocial@gmail.com>
2024-07-26 16:12:12 -0300

    Hi World

--- tests/test_product_api.py
+++ tests/test_product_api.py
@@ -1,8 +1,18 @@
-import requests
+"""import requests
 
 def test_should_get_list_of_products():
     r = requests.get('http://localhost:8000/products')
     response = r.json()
     print(response)
 
-test_should_get_list_of_products()
+test_should_get_list_of_products()"""
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_should_get_list_of_products():
+    response = client.get("/products")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
