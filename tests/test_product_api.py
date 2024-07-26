1e363bbbd7eedd546ffee98c46f6196c5a901b77
unknown <lucasvinisocial@gmail.com>
2024-07-26 20:58:17 -0300

    mod 4 test

--- tests/test_product_api.py
+++ tests/test_product_api.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 def test_should_get_list_of_products():
@@ -9,3 +10,9 @@ def test_should_get_list_of_products():
 
 
 test_should_get_list_of_products()
+
+
+def test_should_get_list_of_products():
+    time.sleep(10)  # Espera 10 segundos antes de tentar acessar a API
+    r = requests.get('http://localhost:8000/products')
+    assert r.status_code == 200
